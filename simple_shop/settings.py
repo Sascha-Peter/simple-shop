da4d28e19e5b2caf748b333553719f838d5f89d3
Sascha Peter <sascha.peter@stickyboard.co.uk>
2015-06-13 12:09:50 +0100

    fix: dev: Add settings and adjust urls to import settings and serve media

--- simple_shop/settings.py
+++ simple_shop/settings.py
@@ -79,3 +79,5 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+from .settings_local import *
--- simple_shop/urls.py
+++ simple_shop/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-import settings
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
